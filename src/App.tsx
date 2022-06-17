065854c5b529eb6b9cd0744e6e3278a50f08e71d
Luana Sauer <luanasauer23@gmail.com>
2022-06-17 15:00:39 -0300

    feat:implementacao tarefas

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { FormEvent, useState } from 'react';
 
 import './global.css';
 import styles from './App.module.css';
@@ -7,10 +7,58 @@ import { Header } from "./Components/Header";
 import { Clipboard, PlusCircle } from 'phosphor-react';
 import { ItemTask } from './Components/ItemTask';
 
+interface TaskProps {
+  id: number;
+  name: string;
+  done: boolean;
+}
+
 export function App() {
 
   const [task, setTask] = useState('');
-  const [taskList, setTaskList] = useState(['tarefa1', 'tarefa2']);
+  const [taskList, setTaskList] = useState<TaskProps[]>(
+    [
+      { id: 1, name: 'Comprar pão', done: false },
+      { id: 2, name: 'Comprar bolo', done: true }
+    ]);
+
+  function handleTaskChange(id: number) {
+    let newList = [...taskList];
+    for (let i in newList) {
+      if (newList[i].id === id) {
+        console.log(newList[i].id, id);
+
+        newList[i].done = !newList[i].done;
+      }
+    }
+    setTaskList(newList);
+  }
+
+  function handleCreateNewTask(event: FormEvent) {
+    event.preventDefault();
+    let newList = [...taskList];
+    newList.push({
+      id: taskList.length + 1,
+      name: task,
+      done: false
+    });
+    setTaskList(newList);
+    setTask('');
+  }
+
+  function handleDeleteTask(idTaskToDelete: number) {
+    const tasksWithoutDeletedOne = taskList.filter(task => {
+      return task.id !== idTaskToDelete;
+    });
+
+    setTaskList(tasksWithoutDeletedOne);
+  }
+
+  const tasksDone = taskList.filter(function (item) {
+    if (item.done) {
+      return true
+    }
+  });
 
   return (
     <div >
@@ -23,35 +71,46 @@ export function App() {
             onChange={(e) => setTask(e.target.value)}
             value={task}
           />
-          <button type="submit" onClick={() => { }}>
+          <button type="submit" onClick={handleCreateNewTask}>
             <strong> Criar </strong>
             <div><PlusCircle size={16} /></div>
           </button>
         </div>
-        {
-          taskList.length === 0 &&
-          <div className={styles.wrapperTasks}>
-            <div className={styles.wrapperTotalTasks}>
-              <div>
-                <span>Tarefas Criadas</span>
-                <div> 0 </div>
-              </div>
-              <div>
-                <span>Concluídas</span>
-                <div> 0 </div>
-              </div>
+
+        <div className={styles.wrapperTasks}>
+          <div className={styles.wrapperTotalTasks}>
+            <div>
+              <span>Tarefas Criadas</span>
+              <div> {taskList.length} </div>
             </div>
+            <div>
+              <span className={styles.done}>Concluídas</span>
+              <div> {tasksDone.length} de {taskList.length} </div>
+            </div>
+          </div>
+          {
+            taskList.length === 0 &&
             <div className={styles.wrapperNoTasks}>
               <Clipboard size={56} />
               <strong>Você ainda não tem tarefas cadastradas</strong>
               <span>Crie tarefas e organize seus itens a fazer</span>
             </div>
-          </div>
-        }
+
+          }
+        </div>
         {
           taskList.length > 0 &&
           <div>
-            <ItemTask />
+            {
+              taskList.map((taskItem) => (
+                <ItemTask
+                  key={taskItem.id}
+                  task={taskItem}
+                  onChange={handleTaskChange}
+                  onDelete={handleDeleteTask}
+                />
+              ))
+            }
           </div>
         }
       </main>
--- src/Components/ItemTask.tsx
+++ src/Components/ItemTask.tsx
@@ -1,7 +1,56 @@
-export function ItemTask() {
+import { Trash } from 'phosphor-react';
+import styles from './ItemTask.module.css';
+
+interface TaskProps {
+    id: number;
+    name: string;
+    done: boolean;
+}
+interface ItemTaskProps {
+    task: TaskProps
+    onChange: (id: number) => void;
+    onDelete: (id: number) => void;
+}
+
+export function ItemTask({ task, onChange, onDelete }: ItemTaskProps) {
+
+    function handleDeleteComment() {
+        onDelete(task.id)
+    }
     return (
-        <div>
-            <input type="checkbox"  />
+        <div className={styles.task}>
+            <div className={styles.round}>
+                <input
+                    type="checkbox"
+                    id={task.id.toString()}
+                    checked={task.done}
+                    onChange={e => onChange(task.id)}
+                />
+                <label htmlFor={task.id.toString()}></label>
+            </div>
+
+            <label>
+
+                {task.done &&
+                    <>
+                        <del>
+                            {task.name}
+                        </del>
+                    </>
+                }
+
+                {!task.done &&
+                    <>
+                        {task.name}
+                    </>
+                }
+            </label>
+            <button
+                title='Deletar tarefa'
+                onClick={handleDeleteComment}
+            >
+                <Trash size={14} />
+            </button>
         </div>
     );
 }
